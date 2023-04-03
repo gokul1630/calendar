fefe76358c941b283d62605cac2122e149eac648
gokul1630 <gokulgp3167@gmail.com>
2023-04-03 22:03:38 +0530

    Some UI changes. hope you like it :)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ const DAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
 const MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
 
-export const App = () => {
+export type DateType = {
+    dates?: Date[];
+};
+
+export const App = ({ dates = [] }: DateType) => {
     const [dateObj, setDateObj] = useState<Date>(new Date());
 
     const currentYear = dateObj.getFullYear();
@@ -45,61 +49,99 @@ export const App = () => {
     });
 
     currentMonthDates.push(
-        ...[...Array(7 - (lastDayIndex + 1))].map((_, idx) => ({ month: nextMonthName, date: idx + 1 }))
+        ...[...Array(14 - (lastDayIndex + 1))].map((_, idx) => ({ month: nextMonthName, date: idx + 1 }))
     );
+
+    const absentDates = dates.map((item) => ({
+        selectedMonth: MONTHS[item.getMonth()],
+        selectedDate: item.getDate(),
+        selectedYear: item.getFullYear(),
+    }));
+
+    const handleNextMonth = (type?: string) => () => {
+        let month: number = currentMonth;
+        let year: number = currentYear;
+
+        switch (type) {
+            case "year":
+                year += 1;
+                break;
+            default:
+                month += 1;
+        }
+        setDateObj(new Date(year, month, currentMonthDate));
+    };
+
+    const handlePreviousMonth = (type?: string) => () => {
+        let month: number = currentMonth;
+        let year: number = currentYear;
+
+        switch (type) {
+            case "year":
+                year -= 1;
+                break;
+            default:
+                month -= 1;
+        }
+        setDateObj(new Date(year, month, currentMonthDate));
+    };
 
     return (
         <div className="px-2 sm:px-5">
-            <div className="flex justify-center gap-5 my-5">
+            <div className="flex justify-center gap-5 my-5 relative">
                 <h1 className="text-xl font-medium text-center m-auto">
                     {currentMonthName} - {currentYear}
                 </h1>
                 <button
-                    className="bg-fuchsia-500 rounded-full p-2 text-white"
-                    onClick={() => {
-                        if (currentMonth < 1) {
-                            setDateObj((state) => new Date(state.getFullYear(), state.getMonth() - 1, state.getDate()));
-                        } else {
-                            setDateObj((state) => new Date(state.getFullYear(), state.getMonth() - 1, state.getDate()));
-                        }
-                    }}>
-                    Prev
+                    className="text-black text-base sm:text-lg font-bold w-7 h-7 absolute left-1 sm:left-8"
+                    onClick={handlePreviousMonth()}>
+                    &lt;
+                </button>
+                <button
+                    className="text-black text-base sm:text-lg font-bold w-7 h-7 absolute left-12 sm:left-32"
+                    onClick={handlePreviousMonth("year")}>
+                    &lt;&lt;
                 </button>
 
                 <button
-                    className="bg-fuchsia-500 rounded-full p-2 text-white"
-                    onClick={() => {
-                        if (currentMonth > 11) {
-                            setDateObj(new Date(currentYear + 1, currentMonth + 1, currentMonthDate));
-                        } else {
-                            setDateObj(new Date(currentYear, currentMonth + 1, currentMonthDate));
-                        }
-                    }}>
-                    Next
+                    className="text-black text-base sm:text-lg font-bold w-7 h-7 absolute right-12 sm:right-32"
+                    onClick={handleNextMonth("year")}>
+                    &gt;&gt;
+                </button>
+                <button
+                    className="text-black text-base sm:text-lg font-bold w-7 h-7 absolute right-1 sm:right-8"
+                    onClick={handleNextMonth()}>
+                    &gt;
                 </button>
             </div>
-
-            <ul className="text-center list-none w-full grid grid-cols-7 grid-rows-none gap-2 sm:gap-5 lg:gap-10">
+            <ul className="text-center list-none w-full grid grid-cols-7 mb-5 bg-blue-100 rounded-lg py-2 px-1 gap-2 sm:gap-5 lg:gap-10">
                 {DAYS?.map((item) => {
                     return (
-                        <li key={item} className="text-sm md:text-xl font-medium">
+                        <li key={item} className="text-md md:text-xl font-medium">
                             {item}
                         </li>
                     );
                 })}
-
-                {currentMonthDates.map(({ date, month }) => {
-                    const currentDate = date > 0 && date;
+            </ul>
+            <ul className="text-center list-none w-full grid grid-cols-7 gap-2 sm:gap-5 lg:gap-10">
+                {currentMonthDates.slice(0, 42).map(({ date, month }) => {
                     return (
                         <li className="flex justify-center items-center" key={month + date}>
                             <h5
-                                className={`w-4 h-4 sm:w-10 sm:h-10 flex justify-center items-center text-sm sm:text-xl p-4 sm:p-5 border-2 border-solid border-fuchsia-500 rounded-full ${
-                                        currentDate === currentMonthDate && month === currentMonthName
-                                            ? "bg-fuchsia-300 text-white"
-                                            : ""
-                                    }
+                                className={`w-4 h-4 sm:w-10 sm:h-10 flex justify-center items-center text-sm font-medium sm:text-xl p-4 sm:p-5 border-2 border-solid border-blue-500 rounded-full ${
+                                    currentMonthName !== month ? "border-blue-100 text-gray-300" : ""
+                                } ${
+                                    absentDates?.find(
+                                        ({ selectedDate, selectedMonth, selectedYear }) =>
+                                            selectedMonth === month &&
+                                            selectedDate === date &&
+                                            selectedYear === currentYear
+                                    )
+                                        ? "bg-blue-500 text-white border-blue-600"
+                                        : ""
+                                } 
                                 `}>
-                                {date && date}
+                                {date}
                             </h5>
                         </li>
                     );
