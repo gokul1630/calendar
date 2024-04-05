94605c18b410bf1c16f4f0f166c761839511f76b
gokul1630 <gokulgp3167@gmail.com>
2024-04-05 11:38:51 +0530

    chore: setup gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir:"build"
+  },
+  base:"https://gokul1630.github.io/calendar",
   plugins: [react()],
 })
